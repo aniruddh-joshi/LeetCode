4e70a46a4d566aa31e36f5d62ff31cbacd3f90d0
Aniruddh Joshi <118598273+aniruddh-joshi@users.noreply.github.com>
2023-05-16 19:02:49 +0530

    Min Cost Climbing Stairs

---
+++ 0746.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+
+    int solve(vector<int>& cost,int n){
+        int prev2=cost[0];
+        int prev1=cost[1];
+
+        for(int i=2;i<n;i++){
+            int curr=cost[i]+min(prev1,prev2);
+            prev2=prev1;
+            prev1=curr;
+        }
+        return min(prev1,prev2);
+    }
+    
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n=cost.size();
+        return solve(cost,n);
+    }
+};
