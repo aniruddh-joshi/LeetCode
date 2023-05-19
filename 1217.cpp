f142d59022df5fa960bd86a332d70c0cbf35fcd2
Aniruddh Joshi <118598273+aniruddh-joshi@users.noreply.github.com>
2023-05-19 14:38:17 +0530

    Minimum Cost to Move Chips to The Same Position

---
+++ 1217.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& position) {
+        int even_count=0;
+        int odd_count=0;
+
+        for(int i=0;i<position.size();i++){
+            if(position[i]%2==0){
+                even_count++;
+            }
+            else{
+                odd_count++;
+            }
+        }
+        return min(even_count,odd_count);
+    }
+};
