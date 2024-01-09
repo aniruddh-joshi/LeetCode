fa6ca7924e00c9b7d83a0891ca6b6e71a74588c9
Aniruddh Joshi <118598273+aniruddh-joshi@users.noreply.github.com>
2024-01-09 17:43:27 +0530

    Leaf-Similar Trees

---
+++ 0872.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int>leaves1,leaves2;
+        findLeaves(root1,leaves1);
+        findLeaves(root2,leaves2);
+
+        return leaves1==leaves2;
+    }
+    void findLeaves(TreeNode* root, vector<int>& leaves) {
+        if (root == NULL) return;
+
+        if (root->left == NULL && root->right == NULL) {
+            leaves.push_back(root->val);
+        }
+
+        findLeaves(root->left, leaves);
+        findLeaves(root->right, leaves);
+    }
+};
