df6209fdf528d15b875c73e1ed651e5324b422bc
Aniruddh Joshi <118598273+aniruddh-joshi@users.noreply.github.com>
2024-01-31 10:11:56 +0530

    Daily Temperatures

---
+++ 0739.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> days(101, 0), ans(n, 0);
+        for(int i=n-1; i>=0; i--){
+            int cur = temperatures[i];
+            fill(days.begin(), days.begin()+cur, i);
+            if(days[cur] != 0) ans[i] = days[cur]-i;
+        }
+        return ans;
+    }
+};
