5c841bc964ef713a924a854952f663f6f6661206
Aniruddh Joshi <118598273+aniruddh-joshi@users.noreply.github.com>
2024-02-16 20:05:32 +0530

    Count Elements With Maximum Frequency

---
+++ 3005.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for(int i = 0; i < nums.size(); i++){
+            mp[nums[i]]++;
+        }
+        int ans = 0;    //Total count of elements with maximum
+        int temp = 0;     //Current maximum frequency
+
+        for(int i = 1; i <= 100; i++){ // Start from 1 instead of 0 as per your logic
+            if(mp[i] == temp){
+                ans += mp[i];
+            }
+            else if(mp[i] > temp){
+                ans = mp[i];
+                temp = mp[i];
+            }
+        }
+        return ans;
+    }
+};
