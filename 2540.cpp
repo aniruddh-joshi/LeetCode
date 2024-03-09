25cbb3a78ca2993fde05292ee93660374e06e6e1
Aniruddh Joshi <118598273+aniruddh-joshi@users.noreply.github.com>
2024-03-09 08:43:08 +0530

    Minimum Common Value

---
+++ 2540.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        for (int i = 0, j = 0; i < n && j < m;) {
+            if (nums1[i] == nums2[j])       return nums1[i];
+            else if (nums1[i] < nums2[j])       i++;
+            else        j++;
+        }
+        return -1;
+    }
+};
