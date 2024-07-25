c225f788c9415189a0b913cd98ac831ca2bcb72c
Aniruddh Joshi <118598273+aniruddh-joshi@users.noreply.github.com>
2024-07-25 09:38:15 +0530

    Time: 94 ms (81.56%), Space: 70.5 MB (68.95%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return nums;
+    }
+};
